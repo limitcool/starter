2c9c6c449b5be4294559ee714f334d8fbef5694c
limitcool <initcoool@gmail.com>
2024-10-29 11:24:07 +0800

    refactor(routers): update router to use controller package

--- routers/router.go
+++ routers/router.go
@@ -2,7 +2,7 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/limitcool/starter/internal/handlers"
+	"github.com/limitcool/starter/internal/controller"
 	"github.com/limitcool/starter/internal/middleware"
 )
 
@@ -19,7 +19,7 @@ func NewRouter() *gin.Engine {
 	// }
 	auth := apiV1.Use(middleware.AuthMiddleware())
 	{
-		auth.GET("/ping", handlers.Ping)
+		auth.GET("/ping", controller.Ping)
 	}
 	return g
 }
