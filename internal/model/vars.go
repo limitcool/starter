2c8f2b648080b0643ab7a58e47351c6937d4fd7b
limitcool <initcoool@gmail.com>
2025-04-11 19:16:37 +0800

    feat:  Modify the era of Snowflake ID to January 1, 2025 to reduce the number of timestamp bits and ensure that the generated ID is within the JavaScript safe integer range.

--- internal/model/vars.go
+++ internal/model/vars.go
@@ -18,6 +18,11 @@ var snowflakeNode *snowflake.Node
 // 初始化雪花ID节点
 func init() {
 	var err error
+	// 修改雪花ID的纪元为当前时间，这样可以减少时间戳部分的位数
+	// 将纪元设置为2025年1月1日，这样可以减少时间戳部分的位数
+	snowflake.Epoch = time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC).UnixMilli()
+
+	// 创建雪花ID节点
 	snowflakeNode, err = snowflake.NewNode(1) // 使用节点ID 1
 	if err != nil {
 		panic(err)
@@ -26,7 +31,18 @@ func init() {
 
 // GenerateSnowflakeID 生成雪花ID
 func GenerateSnowflakeID() int64 {
-	return snowflakeNode.Generate().Int64()
+	// 生成雪花ID
+	id := snowflakeNode.Generate().Int64()
+
+	// 确保生成的ID小于2^53-1 (JavaScript安全整数最大值)
+	// 如果ID大于2^53-1，则取模使其小于2^53-1
+	const maxSafeInt = 9007199254740991 // 2^53-1
+	if id > maxSafeInt {
+		id = id % maxSafeInt
+		log.Info("Snowflake ID exceeds JavaScript safe integer range, taking modulo", "original_id", id, "new_id", id)
+	}
+
+	return id
 }
 
 // GenerateUUID 生成UUID
@@ -91,7 +107,7 @@ type UUIDModel struct {
 func (m *SnowflakeModel) BeforeCreate(tx *gorm.DB) error {
 	if m.ID == 0 {
 		m.ID = GenerateSnowflakeID()
-		log.Info("生成雪花ID", "id", m.ID)
+		log.Info("Generated snowflake ID", "id", m.ID)
 	}
 	return nil
 }
